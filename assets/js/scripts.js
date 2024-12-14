d9e9355c554b9531aebcbe5a11a8fa08d512bb9f
Thulio Schuenck <thulio.schuenck@gmail.com>
2024-12-14 16:47:35 -0300

    feat: add cestions main and skills

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  if (currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
+  else rootHtml.setAttribute("data-theme", "dark");
+
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
